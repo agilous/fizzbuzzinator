27b20e895242a5edd2475da91056c6878172f55a
Bill Barnett <wm.barnett@gmail.com>
2021-04-15 08:41:20 -0400

    Ruby version bump and more comprehensive test

--- fizzbuzzinator_spec.rb
+++ fizzbuzzinator_spec.rb
@@ -7,6 +7,20 @@ RSpec.describe Fizzbuzzinator, type: :model do
   let(:buzz) { 'Buzz' }
   let(:fizzbuzz) { 'Fizzbuzz' }
 
+  it 'produces the expected result' do
+    expected_result = [
+      1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 'Fizz',
+      14, 'Fizzbuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 'Fizz', 'Fizz',
+      'Buzz'
+    ]
+
+    result = (1..25).map do |number|
+      Fizzbuzzinator.fizzbuzzinate(number)
+    end
+
+    expect(result).to eq(expected_result)
+  end
+
   context 'A fizz is when' do
     it 'the number is divisible by 3' do
       expect(Fizzbuzzinator.fizzbuzzinate(3)).to eq(fizz)
